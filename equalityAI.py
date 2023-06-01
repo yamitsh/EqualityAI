12e9e550da3b0beaf58468a369c8be21afb4ee46
yamit <yamitm221@gmail.com>
2023-06-01 18:13:38 +0300

    initial files

---
+++ equalityAI.py
@@ -0,0 +1,23 @@
+class EqualityAI(object):
+    def __init__(self, prompt):
+        self.prompt = prompt
+
+    def get_images(self):
+        pass
+
+    def check_gender(self):
+        pass
+
+    def fix_results(self):
+        pass
+
+    def generate_output(self):
+        pass
+
+
+if __name__ == '__main__':
+    prompt = 'a photograph of a doctor taking a selfie'
+    # prompt = input("Enter your prompt: ")
+    num_images = 3
+
+    eq_ai = EqualityAI(prompt)
